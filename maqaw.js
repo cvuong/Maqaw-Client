d092c35b47ebf352f78d0629806834c9681bdd2f
Eli Hart <elihart@stanford.edu>
2013-07-23 11:46:21 -0700

    Fix back button on login page

--- maqaw.js
+++ maqaw.js
@@ -4,7 +4,7 @@
 
 // start by creating the display
 // pass true to start the client minimized, or false to default to maximize
-var maqawDisplay = new MaqawDisplay(true);
+var maqawDisplay = new MaqawDisplay(false);
 maqawDisplay.setup();
 
 // Initialize the MaqawManager to deal with clients and representatives
