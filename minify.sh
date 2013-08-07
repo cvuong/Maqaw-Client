23de3581a41640276ce76b329b5836bd05b8d8f2
Charlie Vuong <cvuong90@gmail.com>
2013-08-07 10:19:08 -0700

    minifier changes

--- minify.sh
+++ minify.sh
@@ -2,12 +2,12 @@
 
 #must cat them in a specific order because of dependencies 
 
-cat class-ChatManager.js class-LoginPage.js peer.js maqaw-toolbelt.js class-ChatSession.js class-VisitorSession.js class-MaqawManager.js class-MaqawManager.js class-MaqawDisplay.js class-Representative.js class-RepSession.js class-Visitor.js class-VisitorList.js maqaw.js > minifier_temp.js
+cat mirror/tree_mirror.js mirror/class-MaqawMirror.js mirror/mutation_summary.js class-ChatManager.js class-Connection.js class-ConnectionManager.js class-LoginPage.js peer.js maqaw-toolbelt.js class-ChatSession.js class-VisitorSession.js class-MaqawManager.js class-MaqawDisplay.js class-Representative.js class-RepSession.js class-Visitor.js class-VisitorList.js maqaw.js > minifier_temp.js
 
 cat  maqaw.css > minifier_temp.css
 
-yui-compressor minifier_temp.css > maqaw.min.css
-yui-compressor minifier_temp.js > maqaw.min.js 
+#yui-compressor minifier_temp.css > maqaw.min.css
+#yui-compressor minifier_temp.js > maqaw.min.js 
 
 #remove temp file
 #rm minifier_temp.*
